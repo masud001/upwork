48bff4685beef4ebb2980b20b790516b3bba54c7
masud001 <madhnagar@gmail.com>
2020-10-16 13:03:24 +0600

    ..........

--- dashboard/js/charts/dashboard.js
+++ dashboard/js/charts/dashboard.js
@@ -41,8 +41,153 @@ $(document).ready(function () {
 		setting__tools_area.removeClass("visible");
 	});
 
-	// datepicker js
+	// datepicker js one
 
-	// $("#datetimepicker01").datetimepicker();
-	$("#datetimepicker02").datetimepicker();
+	$input1 = $(".select_date1");
+	var options = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_1",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input1.datepicker(options);
+	$input1.data("datepicker").hide = function () {};
+	$input1.datepicker("show");
+
+	// datepicker js two
+
+	$input2 = $(".select_date2");
+	var options2 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_2",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input2.datepicker(options2);
+	$input2.data("datepicker").hide = function () {};
+	$input2.datepicker("show");
+
+	// datepicker js three
+
+	$input3 = $(".select_date3");
+	var options3 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_3",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input3.datepicker(options3);
+	$input3.data("datepicker").hide = function () {};
+	$input3.datepicker("show");
+
+	// datepicker js 4
+
+	$input4 = $(".select_date4");
+	var options4 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_4",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input4.datepicker(options4);
+	$input4.data("datepicker").hide = function () {};
+	$input4.datepicker("show");
+
+	// datepicker js 5
+
+	$input5 = $(".select_date5");
+	var options5 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_5",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input5.datepicker(options5);
+	$input5.data("datepicker").hide = function () {};
+	$input5.datepicker("show");
+
+	// datepicker js 6
+
+	$input6 = $(".select_date6");
+	var options6 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_6",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input6.datepicker(options6);
+	$input6.data("datepicker").hide = function () {};
+	$input6.datepicker("show");
+
+	// datepicker js 7
+
+	$input7 = $(".select_date7");
+	var options7 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_7",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input7.datepicker(options7);
+	$input7.data("datepicker").hide = function () {};
+	$input7.datepicker("show");
+
+	// datepicker js 8
+
+	$input8 = $(".select_date8");
+	var options8 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_8",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input8.datepicker(options8);
+	$input8.data("datepicker").hide = function () {};
+	$input8.datepicker("show");
+
+	// datepicker js 9
+
+	$input9 = $(".select_date9");
+	var options9 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_9",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input9.datepicker(options9);
+	$input9.data("datepicker").hide = function () {};
+	$input9.datepicker("show");
+
+	// datepicker js 10
+
+	$input10 = $(".select_date10");
+	var options10 = {
+		format: "mm/dd/yyyy",
+		container: "#custom__pos_10",
+		todayHighlight: true,
+		autoclose: true,
+		orientation: "bottom",
+	};
+	$input10.datepicker(options10);
+	$input10.data("datepicker").hide = function () {};
+	$input10.datepicker("show");
+
+	// $("#datetimepicker02").datetimepicker();
+
+	$("ul.legend__buttons li a").on("click", function (e) {
+		e.preventDefault();
+	});
+	$("ul.legend__buttons li").on("click", function () {
+		$(this).parent().find("li.active").removeClass("active");
+		$(this).addClass("active");
+	});
 });
--- dashboard/js/charts/equity.js
+++ dashboard/js/charts/equity.js
@@ -86,6 +86,84 @@ const equityChart = new Chart(balancesChart, {
 		],
 	},
 	options: {
+		plugins: {
+			labels: {
+				// render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
+				render: function (args) {
+					// args will be something like:
+					// { label: 'Label', value: 123, percentage: 50, index: 0, dataset: {...} }
+					return args.value;
+
+					// return object if it is image
+					// return { src: 'image.png', width: 16, height: 16 };
+				},
+
+				// precision for percentage, default is 0
+				precision: 0,
+				// identifies whether or not labels of value 0 are displayed, default is false
+				showZero: true,
+
+				// font size, default is defaultFontSize
+				fontSize: 12,
+
+				// font color, can be color array for each data or function for dynamic color, default is defaultFontColor
+				fontColor: ["#E84B4B", "#39E96C", "#FC974B", "#3b7193"],
+
+				// font style, default is defaultFontStyle
+				fontStyle: "normal",
+
+				// font family, default is defaultFontFamily
+				fontFamily: "'Neuton', serif",
+
+				// draw text shadows under labels, default is false
+				textShadow: true,
+
+				// text shadow intensity, default is 6
+				shadowBlur: 10,
+
+				// text shadow X offset, default is 3
+				shadowOffsetX: -5,
+
+				// text shadow Y offset, default is 3
+				shadowOffsetY: 5,
+
+				// text shadow color, default is 'rgba(0,0,0,0.3)'
+				// shadowColor: "rgba(255,0,0,0.75)",
+
+				// draw label in arc, default is false
+				// bar chart ignores this
+				arc: true,
+
+				// position to draw label, available value is 'default', 'border' and 'outside'
+				// bar chart ignores this
+				// default is 'default'
+				position: "outside",
+
+				// draw label even it's overlap, default is true
+				// bar chart ignores this
+				overlap: true,
+
+				// show the real calculated percentages from the values and don't apply the additional logic to fit the percentages to 100 in total, default is false
+				showActualPercentages: true,
+
+				// set images when `render` is 'image'
+				images: [
+					{
+						src: "image.png",
+						width: 16,
+						height: 16,
+					},
+				],
+
+				// add padding when position is `outside`
+				// default is 2
+				outsidePadding: 0,
+
+				// add margin of text when position is `outside` or `border`
+				// default is 2
+				textMargin: 0,
+			},
+		},
 		tooltips: { enabled: false },
 		hover: { mode: null },
 		responsive: true,
@@ -95,6 +173,7 @@ const equityChart = new Chart(balancesChart, {
 			},
 		},
 		legend: {
+			display: true,
 			position: "bottom",
 			align: "center",
 			labels: {
