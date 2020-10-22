f95831f6c504b21cc53d66ccb5f40cd27c8aa2b9
masud001 <madhnagar@gmail.com>
2020-10-23 01:54:34 +0600

    changes font image etc

--- huw-full/js/scripts.js
+++ huw-full/js/scripts.js
@@ -151,10 +151,4 @@ $(document).ready(function () {
 		);
 	});
  */
-	var timeleft = 10;
-	var downloadTimer = setInterval(function () {
-		timeleft--;
-		document.getElementById("countdowntimer").textContent = timeleft;
-		if (timeleft <= 0) clearInterval(downloadTimer);
-	}, 1000);
 });
