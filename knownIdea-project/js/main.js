de5a2dad52be36842f55571082ec97c863e58ca1
masud001 <madhnagar@gmail.com>
2020-11-05 11:09:23 +0600

    knowIdea site design part done

--- knownIdea-project/js/main.js
+++ knownIdea-project/js/main.js
@@ -1,9 +1,36 @@
 $(document).ready(function () {
 	var menu_btn = $(".menu_btn");
+	var close__btn = $(".close__btn");
 	var side__menu_section = $(".side__menu_section");
 	// Burger menu
 	menu_btn.click(function () {
 		side__menu_section.toggleClass("active");
 		menu_btn.toggleClass("active");
+	});
+	close__btn.click(function () {
+		side__menu_section.removeClass("active");
+	});
+
+	$(".hero__slider").owlCarousel({
+		loop: true,
+		rtl: false,
+		margin: 10,
+		responsiveClass: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1,
+				nav: false,
+			},
+			600: {
+				items: 1,
+				nav: false,
+			},
+			1000: {
+				items: 1,
+				nav: false,
+				loop: false,
+			},
+		},
 	});
 });
