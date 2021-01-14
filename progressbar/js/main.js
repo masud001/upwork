c22d71c1678728bebf685b9bd6488fd72942256a
masud001 <madhnagar@gmail.com>
2021-01-14 20:16:13 +0600

    stop click finction

--- progressbar/js/main.js
+++ progressbar/js/main.js
@@ -1,10 +1,10 @@
-$(".steps").on("click", ".step--active", function () {
+$("#steps").on("click", ".step--active", function () {
 	$(this).removeClass("step--incomplete").addClass("step--complete");
 	$(this).removeClass("step--active").addClass("step--inactive");
 	$(this).next().removeClass("step--inactive").addClass("step--active");
 });
 
-$(".steps").on("click", ".step--complete", function () {
+$("#steps").on("click", ".step--complete", function () {
 	$(this).removeClass("step--complete").addClass("step--incomplete");
 	$(this).removeClass("step--inactive").addClass("step--active");
 	$(this)
